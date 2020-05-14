6fd34befaed1d863baa13a11222551905003c809
Neimerc <Neil_Mercury@yahoo.es>
2020-05-14 20:25:11 +0200

    added main application

---
+++ src/main/java/com/neimerc/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,44 @@
+package com.neimerc.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.neimerc.hibernate.demo.entity.Student;
+
+public class CreateStudentDemo {
+
+	public static void main(String[] args) {
+
+		// create factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// create object
+			Student tempStudent = new Student("José Manuel", "Muñoz Manzano", "xxxx@b.com");
+		
+			// begin a transaction
+			session.beginTransaction();
+		
+			// saves the object
+			System.out.println("Saving the student...");
+			session.persist(tempStudent);
+		
+			// commit the transaction
+			session.getTransaction().commit();
+			System.out.println("Done!");
+			
+		} finally {		
+			// clean up code
+			session.close();
+			factory.close();
+		}
+	}
+
+}
